e1e9df4509b530d167ad7d47f0f6398887256017
CodeKittyAlex <133362511+CodeKittyAlex@users.noreply.github.com>
2026-02-03 15:54:52 +0200

    first lesson

---
+++ MaxMinvarages/MaxMinvarages/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MaxMinAvarages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("list numbers");
+
+            int[] numbers = new int[] { 2, 10, 15, 6, 8, 22, 3, 35, 67, 34 };
+
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine(numbers.Min());
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine(numbers.Average());
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("sorteeri numbrid alates väiksemast suremani");
+
+            Array.Sort(numbers);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);    
+            }
+            Console.WriteLine("----------------------");
+            Array.Reverse(numbers);
+            foreach (int value in numbers)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine(Array.BinarySearch(numbers, 2));
+            
+        }
+    }
+}
---
+++ dictsionary/dictsionary/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace dictsionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary ja foreach");
+
+            // var on muutuja
+
+            var domains = new Dictionary<string, string>()
+            {
+                {"fi", "Finland"},
+                {"se", "Sweden"},
+                {"de", "Germany"},
+                {"fr", "France"},
+                {"es", "Spain"},
+            };
+            //kasutate foreachi ja juurde tuleb lisada kolmas rida
+            int i = 0;
+            foreach (var item in domains)
+            {
+                i++;
+                Console.WriteLine($"{item.Key} - {item.Value} - {i}");
+
+
+            }
+        }
+    }
+}
