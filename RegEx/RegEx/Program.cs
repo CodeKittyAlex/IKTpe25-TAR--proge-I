0edc1d32a4c893c494a74992bd80b8d6493b6949
Opilane TTHK <133362511+CodeKittyAlex@users.noreply.github.com>
2026-02-06 10:05:34 +0200

    array lesson

---
+++ RegEx/RegEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("teeme regular EXpresion harjutuse");
+
+            string word = "#CD5C5Cx";
+            Console.WriteLine("hex code: " + word);
+            Console.WriteLine("kas on regex: " + RegExTest(word));
+        }
+
+        public static bool RegExTest(string word)
+        {
+            return Regex.IsMatch(word, @"[#][0-9A-Fa-f]{6}\b");
+        }
+    }
+}
---
+++ arrayAnimalSort/arrayAnimalSort/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace arrayAnimalSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kasutame Array ja Sort-i");
+
+            string[] animals = {"cat", "alligator", "fox",
+                "donkey", "bear", "elephant", "goat" };
+
+            //Array.Sort(animals);
+            foreach (var item in animals)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------------");
+
+            Array.Sort(animals, 0, 3);
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.WriteLine("----------------------");
+            int[] numbers = { 1, 2, 3, 4, 3, 55, 23, 2 };
+
+            int[] distinct = numbers.Distinct().ToArray();
+            foreach (int num in distinct)
+            {
+                Console.WriteLine(num); 
+            }
+        }
+    }
+}
