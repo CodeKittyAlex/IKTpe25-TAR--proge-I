081f7e785435a8eaf944e9779eeabd074f9bd5a2
Opilane TTHK <133362511+CodeKittyAlex@users.noreply.github.com>
2026-02-10 15:55:40 +0200

    lesson 3

---
+++ Enumprojekt/Enumprojekt/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Enumprojekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum");
+
+            Console.WriteLine(Weekday.Friday);
+            Console.WriteLine((int)Weekday.Friday);
+
+            Console.WriteLine("------------");
+            //(int) - castimine e teisendab teiseks andmetüübiks
+            //juhul kui info võib kaduma minna ja ei näita soovitud tulemust
+            Console.WriteLine((int)colours.green);
+            Console.WriteLine(colours.green);
+        }
+        enum Weekday
+        {
+            Monday,
+            Tuesday, 
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        enum colours
+        {
+            red = 3,
+            green = 10, 
+            blue = 13,
+            yellow = 5,
+            Black = 1,
+            white = 8
+        }
+    }
+}
---
+++ ListLINQ/ListLINQ/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ListLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("list and LINQ");
+
+
+            IList <Person> person = new List<Person>();
+            {
+                new Person() { Id = 1, Name = "juku", Age = 10 };
+                new Person() { Id = 2, Name = "juhan", Age = 11 };
+                new Person() { Id = 3, Name = "maali", Age = 10 };
+                new Person() { Id = 4, Name = "aksel", Age = 11 };
+            };
+
+            var persons = from p in person
+                         select new
+                         {
+                             Id = p.Id,
+                             Name = p.Name,
+                             Age = p.Age,
+                         };
+            foreach (var item in persons)
+            {
+                Console.WriteLine("id on " + item.Id + " ja nimi on " + item.Name);
+            }
+            Console.WriteLine("-------------------------");
+            var result = person
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Age = x.Age,
+                });
+
+            foreach (var item in person)
+            {
+                Console.WriteLine("id on " + item.Id + " ja nimi on " + item.Name);
+            }
+            Console.WriteLine("------------------------");
+            Console.WriteLine("gruppide kaupa sorteerimine");
+
+            var groupBy = person
+                .GroupBy(x => x.Age);
+
+            foreach (var item in groupBy)
+            {
+                Console.WriteLine("vanuse grupp on: {0}", item.Key);
+            }
+        }
+    }
+}
