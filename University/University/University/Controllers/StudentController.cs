7bef8ea975eaa25dd0d3bc14fcce2769d9631cb8
Opilane TTHK <133362511+CodeKittyAlex@users.noreply.github.com>
2026-04-02 11:30:21 +0300

    help

--- University/University/University/Controllers/StudentController.cs
+++ University/University/University/Controllers/StudentController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
 
 namespace University.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        private readonly UniversityContext _context;
+
+        public StudentController
+            (
+                UniversityContext context
+            )
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Students.ToListAsync();
+
             return View();
         }
     }
