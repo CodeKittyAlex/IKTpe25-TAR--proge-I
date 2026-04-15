a0924e2bb59ff4486fb51ac4c3fd4485fbd12a50
Opilane TTHK <133362511+CodeKittyAlex@users.noreply.github.com>
2026-04-15 09:56:14 +0300

    student index

---
+++ University/University/University/ViewModle/StudentIndexViewModle.cs
@@ -0,0 +1,14 @@
+﻿using University.Models;
+
+namespace University.ViewModle
+{
+    public class StudentIndexViewModle
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
--- University/University/University/obj/Debug/net10.0/University.AssemblyInfo.cs
+++ University/University/University/obj/Debug/net10.0/University.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("University")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8671fd11b0c6c5ad78911c449b81916c0bc119f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f5992e8aa229023b74daa01eac7c4834aa82e7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("University")]
 [assembly: System.Reflection.AssemblyTitleAttribute("University")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
